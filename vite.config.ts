bc5777c9231c7a8791294eb1acb92bbba5483b37
strawberriesandcheese <ulrike.herwig@student.hpi.de>
2024-02-12 19:30:57 +0100

    WIP add library building

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,23 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig( {
-  base: '/three-openll-labels/',
+export default defineConfig( ( configEnv ) => {
+  if ( configEnv.mode === 'production' ) {
+    return {
+      base: '/three-openll-labels/'
+    };
+  }
+
+  if ( configEnv.mode === 'library' ) {
+    return {
+      build: {
+        lib: {
+          entry: resolve( __dirname, 'lib/main.ts' ),
+          formats: [ 'es' ]
+        }
+      }
+    };
+  }
+
+  return {};
 } );
