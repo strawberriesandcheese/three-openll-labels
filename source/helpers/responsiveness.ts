2528f5fb755853238f01835221718d1faf5e4417
strawberriesandcheese <ulrike.herwig@student.hpi.de>
2024-03-11 21:26:28 +0100

    Improve performance for dpi scaled displays

--- source/helpers/responsiveness.ts
+++ source/helpers/responsiveness.ts
@@ -1,9 +1,15 @@
 export function resizeRendererToDisplaySize( renderer: THREE.WebGLRenderer ) {
   const canvas = renderer.domElement;
+  // on high dpi devices, these are reduced
   const width = canvas.clientWidth;
   const height = canvas.clientHeight;
-  const needResize = canvas.width !== width || canvas.height !== height;
+  // adjust with pixel ratio
+  const widthScaled = Math.floor(width * window.devicePixelRatio);
+  const heightScaled = Math.floor(height * window.devicePixelRatio);
+  // check against adjsuted size
+  const needResize = canvas.width !== widthScaled || canvas.height !== heightScaled;
   if ( needResize ) {
+    // rescale with raw size (three seems to adjust this with device pixel ratio)
     renderer.setSize( width, height, false );
   }
   return needResize;
